dd5653f8b0c342421e9ca61b26a891d6552a7aeb
Chris Boese <chris107565@gmail.com>
2015-02-04 15:17:04 -0800

    fixed excel grid

--- dist/scripts/555834f7.services.js
+++ dist/scripts/555834f7.services.js
@@ -1,28 +1,22 @@
 'use strict';
 
-angular.module('schoolApp.services',['base64','ngCookies'])
-  .factory('Service',['$resource','$state','$cookieStore','$http',Service])
-  .factory('ModelService',['$resource',ModelService])
+angular.module('schoolApp.services',['base64'])
+  .factory('Service',['$resource','$state','$http',Service])
   .service('popupService',['$window',popupService])
-  .service('AuthService',['$resource','$base64','$http',AuthService]);
+  .service('AuthService',['$resource','$base64','$http',AuthService])
+  .factory('ContactService',['$resource',ContactService]);
 
 // Rest Service methods 'get','put','delete'
-function Service($resource,$state,$cookieStore,$http) {
+function Service($resource,$state,$http) {
     // $resource will return data from rest-call
-    return $resource('http://oregonask-service.herokuapp.com/api/:table/:id',{table:'@_table',id:'@_id'},{
-    //return $resource('http://0.0.0.0:8080/api/:table/:id',{table:'@_table',id:'@_id'},{
+    //return $resource('http://oregonask-service.herokuapp.com/api/:table/:id',{table:'@_table',id:'@_id'},{
+    return $resource('http://0.0.0.0:8080/api/:table/:id',{table:'@_table',id:'@_id'},{
       // Set headers on Service request
         save: {
           method: 'put'
         }
     })
 };
-
-function ModelService($resource) {
-    return $resource('/models/:model.json', {model:'@_model'}, {
-      query: {method:'get'}
-    });
-}
 
 function popupService($window){
     this.showPopup=function(message){
@@ -34,8 +28,8 @@ function popupService($window){
 // (uses Basic Authentication)
 function AuthService($resource, $base64,$http) {
     this.Authenticate = function(user,pass,key) {
-      return $resource('http://oregonask-service.herokuapp.com/api/login',{}, {
-      //return $resource('http://0.0.0.0:8080/api/login',{}, {
+      //return $resource('http://oregonask-service.herokuapp.com/api/login',{}, {
+      return $resource('http://0.0.0.0:8080/api/login',{}, {
         login: {
             method : 'get',
             headers: {'Authorization':'Base ' + $base64.encode(user + ':' + pass)}
@@ -46,4 +40,12 @@ function AuthService($resource, $base64,$http) {
           }
         })
     }
+};
+
+function ContactService($resource) {
+  return $resource('http://0.0.0.0:8080/api/contactsAPI/:table', {table:'@_table'}, {
+      save: {
+        method: 'put'
+      }
+  })
 };
