388766884db61ea2de5c0862a03efff97d9decfd
Chris Boese <chris107565@gmail.com>
2015-03-11 12:36:14 -0700

    version 1.0.0

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -6,8 +6,8 @@
   // Rest Service methods 'get','put','delete'
   function Service($resource) {
     // $resource will return data from rest-call
-    //return $resource('http://oregonask-service.herokuapp.com/api/:table/:id', {table: '@_table', id: '@_id'}, {
-    return $resource('http://192.168.11.148:8080/api/:table/:id', {table: '@_table', id: '@_id'}, {
+    return $resource('http://oregonask-service.herokuapp.com/api/:table/:id', {table: '@_table', id: '@_id'}, {
+    //return $resource('http://192.168.11.148:8080/api/:table/:id', {table: '@_table', id: '@_id'}, {
       // Set headers on Service request
         query: {
           method: 'get',
@@ -34,8 +34,8 @@
   // (uses Basic Authentication)
   function AuthService($resource, $base64) {
       this.Authenticate = function (user, pass, key) {
-        //return $resource('http://oregonask-service.herokuapp.com/api/login', {}, {
-        return $resource('http://192.168.11.148:8080/api/login', {}, {
+        return $resource('http://oregonask-service.herokuapp.com/api/login', {}, {
+        //return $resource('http://192.168.11.148:8080/api/login', {}, {
           login: {
               method : 'get',
               headers: {'Authorization': 'Base ' + $base64.encode(user + ':' + pass)}
@@ -50,8 +50,8 @@
 
   // Contacts api Service
   function ContactService($resource) {
-    //return $resource('http://oregonask-service.herokuapp.com/api/contactsAPI/:table', {table: '@_table'}, {
-    return $resource('http://192.168.11.148:8080/api/contactsAPI/:table', {table: '@_table'}, {
+    return $resource('http://oregonask-service.herokuapp.com/api/contactsAPI/:table', {table: '@_table'}, {
+    //return $resource('http://192.168.11.148:8080/api/contactsAPI/:table', {table: '@_table'}, {
         save: {
           method: 'put'
         }
